e24211d11c0ff2f6504fc1c6be396844da66133b
Diego Lopez <diego50421@gmail.com>
2025-05-23 15:25:05 -0400

    added debug logging

--- bgprib_to_prometheus/reader.py
+++ bgprib_to_prometheus/reader.py
@@ -1,24 +1,40 @@
 from pygnmi.client import gNMIclient, telemetryParser
 from google.protobuf import json_format
-from .processor import process_update, process_attr_sets, proc_attr_sets, process_routes
+from .processor import proc_attr_sets, process_routes
 import threading
-from prometheus_client import start_http_server
-
+import logging
+logger = logging.getLogger(__name__)
 
 class gnmi_bgp_reader:
-    def __init__(self, host, port, auth_user, auth_pass, insecure, prometheus_port, vrf=None, afi="ipv4"):
+    def __init__(self, host, port, auth_user, auth_pass, insecure, vrf='default', afi="ipv4"):
+        """
+        Initialize the reader class. 
+        Takes:
+            - host: gNMI endpoint
+            - port: gNMI endpoint's port
+            - auth_user: username to authenticate with
+            - auth_pass: password to authenticate with
+            - insecure: whether to use insecure mode or not
+            - vrf: VRF to pull routing information from
+            - afi: address family to pull routing information for
+        Telemetry is pulled using on_change mode, which syncs the entire RIB first, then only provides the updates.
+        It should be noted this may be fairly taxing on both the collector and the router if there are a lot of routes.
+        """
+
         self.host = host
         self.port = port
         self.auth_user = auth_user
         self.auth_pass = auth_pass
         self.insecure = insecure
-        self.prometheus_port = prometheus_port
         self.vrf = vrf
+        logging.info(f"Using arguments: Host: {self.host}, Port:{self.port}, VRF: {self.vrf}, Insecure: {self.insecure}")
         # TODO: Add more AFIs
         if afi == "ipv4":
             self.afi = "IPV4_UNICAST"
-        self.routes_path = f'/network-instances/network-instance[name={self.vrf}]/protocols/protocol[identifier=BGP][name=BGP]/bgp/rib/afi-safis/afi-safi[afi-safi-name=IPV4_UNICAST]/ipv4-unicast/loc-rib/routes'
+        self.routes_path = f'/network-instances/network-instance[name={self.vrf}]/protocols/protocol[identifier=BGP][name=BGP]/bgp/rib/afi-safis/afi-safi[afi-safi-name={self.afi}]/ipv4-unicast/loc-rib/routes'
         self.attr_path = f'/network-instances/network-instance[name={self.vrf}]/protocols/protocol[identifier=BGP][name=BGP]/bgp/rib/attr-sets/attr-set/as-path'
+        logging.debug(f"OpenConfig Routes Path: {self.routes_path}")
+        logging.debug(f"OpenConfig Attributes Path: {self.attr_path}")
         self.subscribe_routes = {
             'subscription': [
                 {
@@ -41,10 +57,13 @@ class gnmi_bgp_reader:
             'mode' : 'stream',
             'encoding': 'proto'
         }
-    def test_gnmi_connection(self):
-        pass
 
     def _route_update_loop(self):
+        """
+        Initiates collection of route metrics from the gNMI endpoint. 
+        Processing is done in the `process_routes` function.
+        """
+        
         with gNMIclient(target= (self.host, self.port), 
                     username= self.auth_user, password = self.auth_pass, insecure=self.insecure) as gc:
             telementry_stream = gc.subscribe(subscribe=self.subscribe_routes)
@@ -53,6 +72,11 @@ class gnmi_bgp_reader:
                 process_routes(telementry_entry)
 
     def _attr_update_loop(self):
+        """
+        Initiates collection of attributes from the gNMI endpoint. 
+        Processing is done in the `proc_attr_sets` function.
+        """
+
         with gNMIclient(target= (self.host, self.port), 
                     username= self.auth_user, password = self.auth_pass, insecure=self.insecure) as gc:
             telementry_stream = gc.subscribe(subscribe=self.subscribe_attr)
@@ -61,7 +85,10 @@ class gnmi_bgp_reader:
                 proc_attr_sets(telementry_entry)
 
     def start(self):
-        start_http_server(self.prometheus_port)
+        """
+        Spawns threads to begin metric collection on the route and attribute paths. 
+        """
+
         threads = [
             threading.Thread(target=self._route_update_loop, daemon=True),
             threading.Thread(target=self._attr_update_loop,  daemon=True),
